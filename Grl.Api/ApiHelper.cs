b02387439c46afa7c7aa3490206ecf9647bc4304
grltechs <91058023+grltechs@users.noreply.github.com>
2021-11-03 12:29:40 +0530

    Added Api Helper

---
+++ Grl.Api/ApiHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace Grl.Api
+{
+    public class ApiHelper
+    {
+        HttpClient client = new HttpClient();
+        /*
+
+
+
+
+
+
+
+
+
+
+
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {
+            //ApiHelper apiHelper = new ApiHelper();
+            //var os = client.GetAsync(url);
+            
+            try
+            {
+                ApiHelper apiHelper = new ApiHelper();
+                apiHelper = client.GetAsync(url);
+                return os1;
+            }
+            catch
+            {
+                // TODO:
+            }
+            //var os = os1;
+
+        }
+        public Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
+        {
+            try
+            {
+                client.PostAsync(url, content);
+            }
+            catch
+            {
+                // TODO:
+            }
+            return;
+        }
+        public Task<HttpResponseMessage> PutAsync(string url, HttpContent content)
+        {
+            try
+            {
+                client.PutAsync(url, content);
+
+            }
+            catch
+            {
+                // TODO
+            }
+            return;
+        }*/
+        
+
+        public void AddAuthentication(string Token)
+        {
+            
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+    }
+    
+}
