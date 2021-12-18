85e7f3ec79eb210d34ac8ecd47605515669eadec
Marksteev N-grl <91110308+MarksteevN@users.noreply.github.com>
2021-12-18 08:03:00 +0530

    Previous Working State with only Load project

--- Grl.TokenGeneration/RegenerateAcc_Token.cs
+++ Grl.TokenGeneration/RegenerateAcc_Token.cs
@@ -16,16 +16,12 @@ namespace Grl.TokenGeneration
         public static string access_token { get; set; }
         public static string jstring { get; set; }
 
-        /// <summary>
-        /// This Method is used to generate Access Token
-        /// </summary>
-        /// <param name="link">The link contains of client id and redirect uri</param>
-        /// <param name="METHOD">Method contains with Get and post</param>
-        /// <returns>It Returns the Generated Access token</returns>
-        public static string RegenerateAccessToken(string link, string METHOD)
+        public static string RegenerateAccessToken(string link)
         {
             HttpClient client = new HttpClient();
+            string METHOD = "POST";
             HttpResponseMessage response = null;
+
             if ("POST".Equals(METHOD))
             {
                 client.BaseAddress = new Uri(link);
@@ -33,37 +29,47 @@ namespace Grl.TokenGeneration
                 var reqParams = new FormUrlEncodedContent(requestParameters);
                 response = client.PostAsync(link, reqParams).GetAwaiter().GetResult();
             }
+
             Console.WriteLine("Response HTTP Status Code : " + response.StatusCode);
+
             string result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
             string jsonString = " " + result;
             JObject jObject = JObject.Parse(jsonString);
+
             access_token = (string)jObject.SelectToken("access_token");
+
+            //Uri api_domain = (Uri)jObject.SelectToken("api_domain");
+            //string token_type = (string)jObject.SelectToken("token_type");
+            //int expires_in = (int)jObject.SelectToken("expires_in");
+
+            //Console.WriteLine("Regenerated Access Token : " + access_token);
+
             client.Dispose();
             return access_token;
         }
-
-        /// <summary>
-        /// This method acts like skeleton for all the other Operations
-        /// </summary>
-        /// <param name="Link">The correct link to get the data</param>
-        /// <param name="Token">Access token generated from the above method</param>
-        /// <param name="METHOD">get and Post methods are available</param>
         public static void Operations(string Link, string Token, string METHOD)
         {
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //string METHOD = "POST";
             HttpResponseMessage response = null;
             if ("GET".Equals(METHOD))
             {
                 response = client.GetAsync(Link).GetAwaiter().GetResult();
+
+                /*Add params if need in any API
+                private string urlParameters = "?param1=yourparamvalue¶m2=yourparamvalue";
+                response = client.GetAsync(urlParameters).Result;
+                */
             }
             else if ("POST".Equals(METHOD))
             {
                 client.BaseAddress = new Uri(Link);
                 var requestParameters = new Dictionary<string, string>();
-                //requestParameters.Add("title", "Bug tig Title");
-                //requestParameters.Add("assignee_name", "Marksteev");
+                requestParameters.Add("title", "Bug tig Title");
+                requestParameters.Add("assignee_name", "Marksteev");
                 var reqParams = new FormUrlEncodedContent(requestParameters);
                 response = client.PostAsync(Link, reqParams).GetAwaiter().GetResult();
             }
@@ -71,11 +77,44 @@ namespace Grl.TokenGeneration
             Console.WriteLine("Response HTTP Status Code : " + response.StatusCode);
             // Response Body
             string Output = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Response Body : " + Output);
+
             jstring = " " + Output;
-            //Console.WriteLine(jstring);
-            //Creates a text file and stores the output(It acts as a temporary file)
-            //string filep = Directory.GetCurrentDirectory();
-            //System.IO.File.WriteAllText(filep + @"\Appdata\Projects.txt", jstring);
+            System.IO.File.WriteAllText(@"E:\Marksteev\Github_Files\GRLZOHO\GRLZOHO\wwwroot\Textfiles\Project.txt", jstring);
+
+            client.Dispose();
+        }
+
+        public static void Operation(string mainlink, string Token)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            string METHOD = "GET";
+            HttpResponseMessage response = null;
+            if ("GET".Equals(METHOD))
+            {
+                response = client.GetAsync(mainlink).GetAwaiter().GetResult();
+
+                /*Add params if need in any API
+                private string urlParameters = "?param1=yourparamvalue¶m2=yourparamvalue";
+                response = client.GetAsync(urlParameters).Result;
+                */
+            }
+            else if ("POST".Equals(METHOD))
+            {
+                client.BaseAddress = new Uri(mainlink);
+                var requestParameters = new Dictionary<string, string>();
+                requestParameters.Add("title", "Bug tig Title");
+                requestParameters.Add("assignee_name", "Marksteev");
+                var reqParams = new FormUrlEncodedContent(requestParameters);
+                response = client.PostAsync(mainlink, reqParams).GetAwaiter().GetResult();
+            }
+            // Response HTTP Status Code
+            Console.WriteLine("Response HTTP Status Code : " + response.StatusCode);
+            // Response Body
+            Console.WriteLine("Response Body : " + response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
             client.Dispose();
         }
     }
--- Grl.TokenGeneration/UserAuthenticationHelper.cs
+++ Grl.TokenGeneration/UserAuthenticationHelper.cs
@@ -15,28 +15,28 @@ namespace Grl.TokenGeneration
     public class UserAuthenticationHelper
     {
         
-        //private static System.Timers.Timer aTimer;
-        //public void Accesstokentimer()
-        //{
-        //    RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
-        //    string acc_token1 = RegenerateAcc_Token.access_token;
-        //    Console.WriteLine("After returned token from Regenerate acc_token" + acc_token1);
-        //    ApiHelper apiHelper = new ApiHelper();
-        //    apiHelper.AddAuthentication(acc_token1);
-        //    RegenerateAcc_Token.Operations(Linksforall.bugsss, Linksforall.Bugss, acc_token1);
-        //    SetTimer();
-        //    Console.ReadLine();
-        //    aTimer.Stop();
-        //    aTimer.Dispose();
-        //}
-        //private static void SetTimer()
-        //{
-        //    aTimer = new System.Timers.Timer(20000);
-        //    //aTimer = new System.Timers.Timer(3600000);
-        //    aTimer.Elapsed += OnTimedEvent;
-        //    aTimer.AutoReset = true;
-        //    aTimer.Enabled = true;
-        //}
+        private static System.Timers.Timer aTimer;
+        public void Accesstokentimer()
+        {
+            RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
+            string acc_token1 = RegenerateAcc_Token.access_token;
+            Console.WriteLine("After returned token from Regenerate acc_token" + acc_token1);
+            ApiHelper apiHelper = new ApiHelper();
+            apiHelper.AddAuthentication(acc_token1);
+            RegenerateAcc_Token.Operations(Linksforall.bugsss, Linksforall.Bugss, acc_token1);
+            SetTimer();
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+        }
+        private static void SetTimer()
+        {
+            aTimer = new System.Timers.Timer(20000);
+            //aTimer = new System.Timers.Timer(3600000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
         public static void Sleep(int waitTimeMilli)
         {
             if (waitTimeMilli <= 0)
@@ -49,29 +49,28 @@ namespace Grl.TokenGeneration
             delayTimer.Start();
             while (i == 0) { };
         }
-        //private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
-        //    while (true)
-        //    {
-        //        RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
-        //        string acc_token1 = RegenerateAcc_Token.access_token;
-        //        Console.WriteLine("After returned token from Regenerate acc_token" + acc_token1);
-        //        ApiHelper apiHelper = new ApiHelper();
-        //        apiHelper.AddAuthentication(acc_token1);
-        //        RegenerateAcc_Token.Operations(Linksforall.bugsss, Linksforall.Bugss, acc_token1);
-        //        Sleep(5000);
-        //    }
-        //    //Console.WriteLine("{0}, {1}, {2}, {3}", access_token, api_domain, token_type, expires_in);
-        //}
-
-
-        /// <summary>
-        /// The function is to get access token from url
-        /// </summary>        
-        //public void ZohoProcess()
-        //{
-        //    RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
-        //}
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            while (true)
+            {
+                RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
+                string acc_token1 = RegenerateAcc_Token.access_token;
+                Console.WriteLine("After returned token from Regenerate acc_token" + acc_token1);
+                ApiHelper apiHelper = new ApiHelper();
+                apiHelper.AddAuthentication(acc_token1);
+                RegenerateAcc_Token.Operations(Linksforall.bugsss, Linksforall.Bugss, acc_token1);
+                Sleep(5000);
+            }
+            //Console.WriteLine("{0}, {1}, {2}, {3}", access_token, api_domain, token_type, expires_in);
+        }
+        public void WithOutTimer()
+        {
+            ApiHelper apiHelper = new ApiHelper();
+            RegenerateAcc_Token.RegenerateAccessToken(Linksforall.GetUrl);
+            //Console.WriteLine(RegenerateAcc_Token.access_token);
+            RegenerateAcc_Token.Operations(Linksforall.AllProjects, RegenerateAcc_Token.access_token, EnumClass.METHOD.GET.ToString());
+            //apiHelper.AddAuthentication()
+        }
 
         
         
